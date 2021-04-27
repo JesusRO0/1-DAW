b880f550f67eb5221127cc22091ad7b6bf8776f6
jesus <jesus@jesusR-VirtualBox>
2021-04-27 13:37:04 +0200

    version 1

---
+++ Interfaces/src/main/java/Personajes/Habilidades.java
@@ -0,0 +1,11 @@
+package Personajes;
+
+
+public interface Habilidades {
+    
+    public void atacar();
+    public void defender();
+    public void poderes();
+    public void talentos();
+    
+}
