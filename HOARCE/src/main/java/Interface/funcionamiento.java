64389c94ba86545177b4dab8a32c4dee2380e870
jesus <jesus@jesusR-VirtualBox>
2021-05-27 13:19:44 +0200

    version 1

---
+++ HOARCE/src/main/java/Interface/funcionamiento.java
@@ -0,0 +1,9 @@
+package Interface;
+
+
+public interface funcionamiento {
+    
+    public String transportar();
+    public String acelerar();
+    public String frenar();
+}
---
+++ HOARCE/src/main/java/SercvicioProduccion/servicioProduccion.java
@@ -0,0 +1,7 @@
+
+package SercvicioProduccion;
+
+
+public class servicioProduccion {
+    
+}
